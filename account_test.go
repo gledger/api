0c8777b7aee405c0127e988b4831bd7fb10fb991
Jeremy Bush <contractfrombelow@gmail.com>
2016-08-18 22:23:36 -0500

    Add domain tests

---
+++ account_test.go
@@ -0,0 +1,25 @@
+package gledger
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CreateAssignsUuid_IfNotExists(t *testing.T) {
+	a := Account{Name: "test"}
+	var saveCalled bool
+	saveErr := errors.New("boom")
+	saveAccount := func(act Account) error {
+		assert.Equal(t, a.Name, act.Name)
+		assert.NotEmpty(t, act.Uuid)
+		saveCalled = true
+		return saveErr
+	}
+
+	svc := accountService{saveAccount: saveAccount}
+	_, err := svc.Create(a)
+	assert.Equal(t, saveErr, err)
+	assert.True(t, saveCalled)
+}
---
+++ transaction_test.go
@@ -0,0 +1,25 @@
+package gledger
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_TransactionCreateAssignsUuid_IfNotExists(t *testing.T) {
+	a := Transaction{Payee: "test"}
+	var saveCalled bool
+	saveErr := errors.New("boom")
+	saveTransaction := func(txn Transaction) error {
+		assert.Equal(t, a.Payee, txn.Payee)
+		assert.NotEmpty(t, txn.Uuid)
+		saveCalled = true
+		return saveErr
+	}
+
+	svc := transactionService{saveTransaction: saveTransaction}
+	_, err := svc.Create(a)
+	assert.Equal(t, saveErr, err)
+	assert.True(t, saveCalled)
+}
