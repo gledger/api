97f4647e51a445ff1dc2b6235954d13bdc03f309
Jeremy Bush <contractfrombelow@gmail.com>
2016-08-24 22:18:28 -0500

    Add balance attribute to accounts, return them from repository

--- db/account_repository.go
+++ db/account_repository.go
@@ -19,7 +19,13 @@ func AllAccounts(db *sql.DB) func() ([]gledger.Account, error) {
 	return func() ([]gledger.Account, error) {
 		var accounts []gledger.Account
 
-		rows, err := db.Query(`SELECT account_uuid, name, type, active FROM accounts`)
+		rows, err := db.Query(
+			`SELECT
+				account_uuid, name, type, active, sum(transactions.amount)
+			FROM accounts
+			LEFT JOIN transactions USING(account_uuid)
+			GROUP BY accounts.account_uuid`,
+		)
 		if err != nil {
 			return accounts, errors.Wrap(err, "error getting all accounts")
 		}
@@ -27,9 +33,14 @@ func AllAccounts(db *sql.DB) func() ([]gledger.Account, error) {
 
 		for rows.Next() {
 			var a gledger.Account
-			err := rows.Scan(&a.Uuid, &a.Name, &a.Type, &a.Active)
+			var b sql.NullInt64
+			err := rows.Scan(&a.Uuid, &a.Name, &a.Type, &a.Active, &b)
 			if err != nil {
 				return accounts, errors.Wrap(err, "error scanning all accounts")
+			}
+
+			if b.Valid {
+				a.Balance = b.Int64
 			}
 
 			accounts = append(accounts, a)
@@ -42,13 +53,27 @@ func AllAccounts(db *sql.DB) func() ([]gledger.Account, error) {
 func ReadAccount(db *sql.DB) func(string) (gledger.Account, error) {
 	return func(uuid string) (gledger.Account, error) {
 		var account gledger.Account
+		var b sql.NullInt64
 
-		err := db.QueryRow(`SELECT account_uuid, name, type, active FROM accounts WHERE account_uuid = $1`, uuid).Scan(
+		err := db.QueryRow(
+			`SELECT
+				account_uuid, name, type, active, sum(transactions.amount)
+			FROM accounts
+			LEFT JOIN transactions USING(account_uuid)
+			WHERE account_uuid = $1
+			GROUP BY accounts.account_uuid`,
+			uuid,
+		).Scan(
 			&account.Uuid,
 			&account.Name,
 			&account.Type,
 			&account.Active,
+			&b,
 		)
+
+		if b.Valid {
+			account.Balance = b.Int64
+		}
 
 		return account, errors.Wrapf(err, "error reading account %s", uuid)
 	}
---
+++ db/account_repository_test.go
@@ -0,0 +1,100 @@
+package db
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+	"github.com/twinj/uuid"
+	"github.com/zombor/gledger"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type AccountRepositorySuite struct {
+	suite.Suite
+
+	db *sql.DB
+}
+
+func (s *AccountRepositorySuite) SetupSuite() {
+	pg, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	s.Require().NoError(err)
+	s.db = pg
+	_, err = s.db.Exec(`TRUNCATE accounts CASCADE`)
+	s.Require().NoError(err)
+}
+
+func (s *AccountRepositorySuite) SetupTest() {
+	_, err := s.db.Exec(`TRUNCATE accounts CASCADE`)
+	s.Require().NoError(err)
+}
+
+func (s *AccountRepositorySuite) Test_ReadAccount() {
+	u := uuid.NewV4().String()
+	s.db.Exec(`INSERT INTO accounts VALUES ($1, 'name', 'type', 't', now(), now())`, u)
+
+	a, err := ReadAccount(s.db)(u)
+	s.NoError(err)
+	s.Equal(gledger.Account{
+		Uuid:    u,
+		Name:    "name",
+		Type:    "type",
+		Active:  true,
+		Balance: 0,
+	}, a)
+}
+
+func (s *AccountRepositorySuite) Test_ReadAccount_WithTransactions() {
+	u := uuid.NewV4().String()
+	s.db.Exec(`INSERT INTO accounts VALUES ($1, 'name', 'type', 't', now(), now())`, u)
+	s.db.Exec(`INSERT INTO transactions VALUES ($1, $2, now(), 'payee', 10, 'f', 'f', now(), now())`, uuid.NewV4(), u)
+	s.db.Exec(`INSERT INTO transactions VALUES ($1, $2, now(), 'payee', -5, 'f', 'f', now(), now())`, uuid.NewV4(), u)
+
+	a, err := ReadAccount(s.db)(u)
+	s.NoError(err)
+	s.Equal(gledger.Account{
+		Uuid:    u,
+		Name:    "name",
+		Type:    "type",
+		Active:  true,
+		Balance: 5,
+	}, a)
+}
+
+func (s *AccountRepositorySuite) Test_AllAccounts() {
+	u := uuid.NewV4().String()
+	s.db.Exec(`INSERT INTO accounts VALUES ($1, 'name', 'type', 't', now(), now())`, u)
+
+	as, err := AllAccounts(s.db)()
+	s.NoError(err)
+	s.Equal([]gledger.Account{gledger.Account{
+		Uuid:    u,
+		Name:    "name",
+		Type:    "type",
+		Active:  true,
+		Balance: 0,
+	}}, as)
+}
+
+func (s *AccountRepositorySuite) Test_AllAccounts_WithTransactions() {
+	u := uuid.NewV4().String()
+	s.db.Exec(`INSERT INTO accounts VALUES ($1, 'name', 'type', 't', now(), now())`, u)
+	s.db.Exec(`INSERT INTO transactions VALUES ($1, $2, now(), 'payee', 10, 'f', 'f', now(), now())`, uuid.NewV4(), u)
+	s.db.Exec(`INSERT INTO transactions VALUES ($1, $2, now(), 'payee', -5, 'f', 'f', now(), now())`, uuid.NewV4(), u)
+
+	as, err := AllAccounts(s.db)()
+	s.NoError(err)
+	s.Equal([]gledger.Account{gledger.Account{
+		Uuid:    u,
+		Name:    "name",
+		Type:    "type",
+		Active:  true,
+		Balance: 5,
+	}}, as)
+}
+
+func Test_AccountRepository(t *testing.T) {
+	suite.Run(t, new(AccountRepositorySuite))
+}
