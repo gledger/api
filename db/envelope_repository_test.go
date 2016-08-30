78723c0f273905b9e11bf7763a79aad874b86870
Jeremy Bush <contractfrombelow@gmail.com>
2016-08-29 21:29:10 -0500

    Read envelope balance when reading all envelopes
    Fixes #22

--- db/envelope_repository_test.go
+++ db/envelope_repository_test.go
@@ -38,23 +38,23 @@ func (s *EnvelopeRepositorySuite) Test_SaveEnvelope() {
 
 func (s *EnvelopeRepositorySuite) Test_AllEnvelopes() {
 	e1 := gledger.Envelope{
-		UUID: uuid.NewV4().String(),
-		Name: "Test Envelope 1",
-		Type: "income",
+		UUID:    uuid.NewV4().String(),
+		Name:    "Test Envelope 1",
+		Type:    "income",
+		Balance: 10,
 	}
 	e2 := gledger.Envelope{
 		UUID: uuid.NewV4().String(),
 		Name: "Test Envelope 2",
 		Type: "expense",
 	}
-	_, err := s.tx.Exec(`INSERT INTO envelopes VALUES (
+	s.mustExec(`INSERT INTO envelopes VALUES (
 		$1, $2, now(), now(), $3
 	)`, e1.UUID, e1.Name, e1.Type)
-	s.NoError(err)
-	_, err = s.tx.Exec(`INSERT INTO envelopes VALUES (
+	s.mustExec(`INSERT INTO envelopes VALUES (
 		$1, $2, now(), now(), $3
 	)`, e2.UUID, e2.Name, e2.Type)
-	s.NoError(err)
+	s.mustExec(`INSERT INTO transactions VALUES ($1, $2, now(), 'payee', 10, 'f', 'f', now(), now(), $3)`, uuid.NewV4(), "cadd0722-6fd1-47ff-b390-b53307cc8c01", e1.UUID)
 
 	es, err := AllEnvelopes(s.tx.Query)()
 	if s.NoError(err) {
@@ -72,10 +72,17 @@ func (s *EnvelopeRepositorySuite) SetupTest() {
 	var err error
 	s.tx, err = s.db.Begin()
 	s.NoError(err)
+
+	s.mustExec(`INSERT INTO accounts VALUES ($1, 'account name', 'type', 't', now(), now())`, "cadd0722-6fd1-47ff-b390-b53307cc8c01")
 }
 
 func (s *EnvelopeRepositorySuite) TearDownTest() {
 	err := s.tx.Rollback()
+	s.NoError(err)
+}
+
+func (s *EnvelopeRepositorySuite) mustExec(query string, args ...interface{}) {
+	_, err := s.tx.Exec(query, args...)
 	s.NoError(err)
 }
 
